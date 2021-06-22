95a7b88a9a1dd386eda85dfe3d17ac706fb26a0c
Vikrant43 <85218465+Vikrant43@users.noreply.github.com>
2021-06-22 12:50:52 +0530

    Update ShiftElementsInArray.java

--- src/main/java/exam/ShiftElementsInArray.java
+++ src/main/java/exam/ShiftElementsInArray.java
@@ -4,7 +4,24 @@ public class ShiftElementsInArray {
 
   public void shift(int[] arr) {
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
-
+         int d=arr.length -1;
+          int n=arr.length;
+          int temp[] = new int[d];
+ 
+        for (int i = 0; i < d; i++)
+            temp[i] = arr[i];
+ 
+        for (int i = d; i < n; i++) {
+            arr[i - d] = arr[i];
+        }
+ 
+  
+        for (int i = 0; i < d; i++) {
+            arr[i + n - d] = temp[i];
+        }
+        for (int i = 0; i < n; i++)
+            System.out.print(arr[i] + ",");
+    
   }
 
 }
